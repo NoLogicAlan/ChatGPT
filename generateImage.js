6b46f91b470eab6cc68729c47575ed4db6e885fd
NoLogicAlan <88591830+NoLogicAlan@users.noreply.github.com>
2023-02-12 20:36:54 +0300

    :>

--- generateImage.js
+++ generateImage.js
@@ -5,8 +5,8 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const rateLimitPeriod = 10 * 1000; // 10 seconds
-const rateLimitCount = 10;
+const RATE_LIMIT_PERIOD = 10 * 1000; // 10 seconds
+const RATE_LIMIT_COUNT = 10;
 
 let rateLimitResetTime = 0;
 let rateLimitCounter = 0;
@@ -14,26 +14,26 @@ let rateLimitCounter = 0;
 async function ask(prompt) {
   try {
     if (Date.now() < rateLimitResetTime) {
-      if (rateLimitCounter >= rateLimitCount) {
-        return { success: false, error: `Rate limit reached. Try again in ${(rateLimitResetTime - Date.now()) / 1000} seconds.` };
+      if (rateLimitCounter >= RATE_LIMIT_COUNT) {
+        throw new Error(`Rate limit reached. Try again in ${(rateLimitResetTime - Date.now()) / 1000} seconds.`);
       }
       rateLimitCounter++;
     } else {
       rateLimitCounter = 0;
-      rateLimitResetTime = Date.now() + rateLimitPeriod;
+      rateLimitResetTime = Date.now() + RATE_LIMIT_PERIOD;
     }
 
     const response = await openai.createImage({
-      prompt: prompt,
+      prompt,
       n: 1,
       size: "1024x1024"
     });
-    const imageUrls = response.data.data.map(e => e.url);
+    const imageUrls = response.data.data.map(({ url }) => url);
 
     return { success: true, imageUrls };
   } catch (error) {
     console.error(error);
-    return { success: false, error: error.message };
+    return { success: false, error: error.message + " or bad prompt (maybe)" };
   }
 }
 
