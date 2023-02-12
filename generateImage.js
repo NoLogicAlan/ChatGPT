beefd22157830f992d32dfed0381c01ad04ef3ea
NoLogicAlan <88591830+NoLogicAlan@users.noreply.github.com>
2023-02-12 20:19:05 +0300

    so yeah idk why i add this but is fun

--- generateImage.js
+++ generateImage.js
@@ -5,14 +5,30 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const rateLimitPeriod = 10 * 1000; // 10 seconds
+const rateLimitCount = 10;
+
+let rateLimitResetTime = 0;
+let rateLimitCounter = 0;
+
 async function ask(prompt) {
   try {
+    if (Date.now() < rateLimitResetTime) {
+      if (rateLimitCounter >= rateLimitCount) {
+        return { success: false, error: `Rate limit reached. Try again in ${(rateLimitResetTime - Date.now()) / 1000} seconds.` };
+      }
+      rateLimitCounter++;
+    } else {
+      rateLimitCounter = 0;
+      rateLimitResetTime = Date.now() + rateLimitPeriod;
+    }
+
     const response = await openai.createImage({
       prompt: prompt,
-      n: 2,
+      n: 1,
       size: "1024x1024"
     });
-    const imageUrls = response.data.data.map(e=>e.url);
+    const imageUrls = response.data.data.map(e => e.url);
 
     return { success: true, imageUrls };
   } catch (error) {
