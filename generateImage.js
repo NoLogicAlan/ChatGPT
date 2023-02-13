d6b1012152a56598aeb194dc9849f0c371bdfc3a
NoLogicAlan <88591830+NoLogicAlan@users.noreply.github.com>
2023-02-13 15:40:34 +0300

    maybe u love it

--- generateImage.js
+++ generateImage.js
@@ -1,5 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 const API_KEY = "its ur problem";
+const DEFAULT_MODEL = "image-alpha-001";
+const AVAILABLE_MODELS = ["image-alpha-001", "image-alpha-002", "image-alpha-003", "image-dall-E-002", "image-curie-001", "image-curie-002", "image-babbage-001", "image-jukebox-001"];
 const configuration = new Configuration({
   apiKey: API_KEY,
 });
@@ -11,7 +13,10 @@ const RATE_LIMIT_COUNT = 10;
 let rateLimitResetTime = 0;
 let rateLimitCounter = 0;
 
-async function ask(prompt) {
+async function ask(prompt, model = DEFAULT_MODEL,) {
+  if (!AVAILABLE_MODELS.includes(model)) {
+    throw new Error(`Error: The model "${model}" is not available. Please choose from: ${AVAILABLE_MODELS.join(", ")}`);
+  }
   try {
     if (Date.now() < rateLimitResetTime) {
       if (rateLimitCounter >= RATE_LIMIT_COUNT) {
@@ -25,6 +30,7 @@ async function ask(prompt) {
 
     const response = await openai.createImage({
       prompt,
+      model,
       n: 1,
       size: "1024x1024"
     });
