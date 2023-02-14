46fe0b12c2bcd3aa92d11af837f56eb3e72fd81b
NoLogicAlan <88591830+NoLogicAlan@users.noreply.github.com>
2023-02-14 18:20:25 +0300

    adding limited shits

--- commands/generate-image.js
+++ commands/generate-image.js
@@ -1,15 +1,5 @@
 const { CommandBuilder } = require("../Commands.js");
 const { ask } = require("../generateImage");
-const Uploader = require("revolt-uploader");
-const https = require('https');
-
-async function upload(url) {
-  return new Promise((res) => {
-    https.get(url, async (response) => {
-      res(await this.upload.upload(response, "generatedImage"));
-    });
-  });
-}
 
 module.exports = {
   command: new CommandBuilder()
@@ -21,23 +11,13 @@ module.exports = {
         .setRequired(true)),
   run: async function (message, data) {
     const prompt = data.get("prompt").value
-    const answer = await ask(prompt);
+    const answer = await ask(prompt, message);
     if (answer.success) {
-      const startTime = new Date();
-      const elapsedTime = (new Date() - startTime) / 1000;
-      //message.channel.sendMessage({
-      //  content: " ",
-      //  images: (await Promise.all(answer.imageUrls.map(i=>upload(i))).map(i=>i.value))
-      //})
-      message.channel.sendMessage(`Image generated in ${elapsedTime} seconds, please wait...`);
-      setTimeout(() => {
-        answer.imageUrls.forEach(url => {
-          message.channel.sendMessage(`[Image](${url})`);
-        });
-      }, 5000); // 5000ms = 5s
+      answer.imageUrls.forEach(url => {
+        message.channel.sendMessage(`[Image](${url})`);
+      });
     } else {
       message.channel.sendMessage(answer.error);
     }
   }
 }
-
--- generateImage.js
+++ generateImage.js
@@ -1,41 +1,86 @@
 const { Configuration, OpenAIApi } = require("openai");
+const fs = require("fs");
+const path = require("path");
+
 const API_KEY = "its ur problem";
 const DEFAULT_MODEL = "image-alpha-001";
-const AVAILABLE_MODELS = ["image-alpha-001", "image-alpha-002", "image-alpha-003", "image-dall-E-002", "image-curie-001", "image-curie-002", "image-babbage-001", "image-jukebox-001"];
+const AVAILABLE_MODELS = [
+  "image-alpha-001",
+  "image-alpha-002",
+  "image-alpha-003",
+  "image-dall-E-002",
+  "image-curie-001",
+  "image-curie-002",
+  "image-babbage-001",
+  "image-jukebox-001",
+];
+
+const RATE_LIMIT_COUNT = 1000;
+
+const usageDataPath = path.join(__dirname, "usageData.json");
+
 const configuration = new Configuration({
   apiKey: API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
-const RATE_LIMIT_PERIOD = 10 * 1000; // 10 seconds
-const RATE_LIMIT_COUNT = 10;
+let usageData = {};
 
-let rateLimitResetTime = 0;
-let rateLimitCounter = 0;
+// Load the usage data from the file
+try {
+  const rawData = fs.readFileSync(usageDataPath);
+  usageData = JSON.parse(rawData);
+} catch (error) {
+  console.error("Failed to load usage data from file", error);
+}
 
-async function ask(prompt, model = DEFAULT_MODEL,) {
+// Save the usage data to the file
+function saveUsageData() {
+  fs.writeFileSync(usageDataPath, JSON.stringify(usageData));
+}
+
+async function ask(prompt, message, model = DEFAULT_MODEL) {
+  let userId = null;
+  if (message && message.author_id) {
+    userId = message.author_id;
+  }
   if (!AVAILABLE_MODELS.includes(model)) {
     throw new Error(`Error: The model "${model}" is not available. Please choose from: ${AVAILABLE_MODELS.join(", ")}`);
   }
-  try {
-    if (Date.now() < rateLimitResetTime) {
-      if (rateLimitCounter >= RATE_LIMIT_COUNT) {
-        throw new Error(`Rate limit reached. Try again in ${(rateLimitResetTime - Date.now()) / 1000} seconds.`);
-      }
-      rateLimitCounter++;
-    } else {
-      rateLimitCounter = 0;
-      rateLimitResetTime = Date.now() + RATE_LIMIT_PERIOD;
-    }
 
+  const now = Date.now();
+  const userUsage = usageData[userId] || { count: 0, resetTime: 0 };
+
+  // Check if the user has exceeded the usage limit
+  if (userUsage.count >= RATE_LIMIT_COUNT && now < userUsage.resetTime) {
+    const timeLeft = Math.ceil((userUsage.resetTime - now) / 1000);
+    throw new Error(`Usage limit reached for user ${userId}. Try again in ${timeLeft} seconds.`);
+  }
+
+  userUsage.count++;
+
+  // Check if the user has used the API 1000 times
+  if (userUsage.count === 1000) {
+    userUsage.limitReached = true;
+  }
+
+  // Save the updated usage data to the file
+  usageData[userId] = userUsage;
+  saveUsageData();
+
+  if (userUsage.limitReached) {
+    return { success: false, error: "User" + `<@${userId}>` + "has reached 1000 API uses." };
+  }
+
+  try {
     const response = await openai.createImage({
       prompt,
       model,
       n: 1,
       size: "1024x1024"
     });
-    const imageUrls = response.data.data.map(({ url }) => url);
-
+    const imageUrls = response?.data?.data?.map(({ url }) => url);
     return { success: true, imageUrls };
   } catch (error) {
     console.error(error);
